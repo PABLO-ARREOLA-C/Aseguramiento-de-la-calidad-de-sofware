5dd8cea4b68197fe209b3130ddd25e2e0bd34978
Pablo-Arreola <parreolac7@gmail.com>
2026-08-29 11:08:29 -0600

    clase 7

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,28 +1,29 @@
+// playwright.config.ts
+
 import { defineConfig, devices } from '@playwright/test';
-
 export default defineConfig({
-  testDir: './tests',
-  timeout: 30000,
-
-  reporter: [
-    ['list'],
-    ['html', { open: 'never' }],
-  ],
-
-  use: {
-    baseURL: 'https://www.demoblaze.com',
-    headless: false,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-  },
-
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        browserName: 'chromium',
-        ...devices['Desktop Chrome'],
-      },
+    testDir: './tests',
+    timeout: 30000,
+    retries: 1,           // reintentar tests fallidos 1 vez
+    use: {
+        baseURL: 'https://www.saucedemo.com',
+        headless: true,
+        screenshot: 'on',   // 'on' | 'off' | 'only-on-failure'
+        video: 'on',        // 'on' | 'off' | 'retain-on-failure'
+        trace: 'on',        // 'on' | 'off' | 'retain-on-failure'
     },
-  ],
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['chromium'] },
+        },
+        {
+            name: 'firefox',
+            use: { ...devices['firefox'] },
+        },
+        {
+            name: 'webkit',
+            use: { ...devices['webkit'] },
+        },
+    ],
 });
